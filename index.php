e8d17e3176dc01f2e3cb0b0ce0913da686e4f880
Jairo-Arcy <jairo_tunisse@outlook.com>
2021-04-20 21:53:41 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Message</title>
+</head>
+<body>
+        <label for="lblMessage">Digite aqui sua mensagem:</label>
+        <input type="text" name="lblMessage" id="lblMessage"/>
+        
+
+
+</body>
+</html>
