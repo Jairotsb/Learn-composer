587d5655fa5dd4035dc79094f8871f19a8bfc279
Jairo-Arcy <jairo_tunisse@outlook.com>
2021-04-21 15:55:44 -0300

    list supports feature added

--- index.php
+++ index.php
@@ -1,33 +1,30 @@
-<?php 
-    session_start();
-    if(!isset($_SESSION['user_name'])){
-        header("Location: pages/login/");
-    }
+<?php
+session_start();
+if (!isset($_SESSION['user_name'])) {
+    header("Location: pages/login/");
+}
 ?>
 
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Message</title>
+    <link rel="stylesheet" href="src/index.css">
 </head>
+
 <body>
-        
-        <input type="hidden" id="lblName" value="<?= $_SESSION['user_name'] ?>"/>
-
-
-        <label for="message">Digite aqui sua mensagem:</label>
-        <input type="text" name="message" id="lblMessage"/>
-        
-
-        <div id="lblResponse"></div>
-
+        <div class="input-message">
+            <input type="text" name="message" class="message" id="lblMessage" />
+        </div><input type="hidden" id="lblName" value="<?= $_SESSION['user_name'] ?>" />
         <a href="src/php/exit.php">SAIR</a>
-
-
-        <script src="src/index.js"></script> 
+        <div class="messages-box" id="lblResponse"></div>
+    
+        <script src="src/index.js"></script>
 </body>
+
 </html>
